61414a4868466aab0e9933f31bed7bbcc932861b
d-a-n-i <dani.85@gmail.com>
2026-09-24 01:53:51 +0200

    Same-offset timezones show one time (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'land-v4';
+const V = 'land-v5';
 const SHELL = ['./','index.html','style.css','app.js','airports.json','manifest.webmanifest','icon.svg','icon-192.png','icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k!==V).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
